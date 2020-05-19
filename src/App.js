fb98d1187e33f4cebf9bde50e8e04ea8410d8754
Aleksuo <aleksi.suoranta@gmail.com>
2020-05-19 22:05:56 +0300

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -74,12 +74,12 @@ function Card(props) {
   )
 }
 function App() {
-  const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 })
-  const isBigScreen = useMediaQuery({ minDeviceWidth: 1824 })
+  //const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 })
+  //const isBigScreen = useMediaQuery({ minDeviceWidth: 1824 })
   const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
-  const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 })
-  const isPortrait = useMediaQuery({ orientation: 'portrait' })
-  const isRetina = useMediaQuery({ minResolution: '2dppx' })
+  //const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 })
+  //const isPortrait = useMediaQuery({ orientation: 'portrait' })
+  //const isRetina = useMediaQuery({ minResolution: '2dppx' })
  
   return (
     <div className="container">
