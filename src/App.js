81e2cf85361decd51ab5fb73f97d1ed7371e6093
Aleksuo <aleksi.suoranta@gmail.com>
2020-05-20 01:35:44 +0300

    fixed references

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useMediaQuery } from 'react-responsive'
 import './style.scss';
 
 import Carousel from '@brainhubeu/react-carousel';
@@ -19,7 +18,7 @@ const socialIcons = {
 function IconRow() {
   return (
     <span style={socialIcons}>
-      <a href="https://github.com/Aleksuo" target="_blank"><i className="fab fa-github" ></i></a>
+      <a href="https://github.com/Aleksuo" target="_blank" rel="noopener noreferrer"><i className="fab fa-github" ></i></a>
       <i className="fab fa-linkedin"></i>
       <i className="far fa-file"></i>
     </span>
