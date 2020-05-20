39cb94e52793d3e4187673a75762678c085e72ce
Aleksuo <aleksi.suoranta@gmail.com>
2020-05-20 14:11:49 +0300

    removed duplicate style

--- src/App.js
+++ src/App.js
@@ -9,13 +9,9 @@ const accentStyle = {
 }
 
 
-const socialIcons = {
-  fontSize: "48px"
-}
-
 function IconRow() {
   return (
-    <span style={socialIcons}>
+    <span className="huge">
       <a href="https://github.com/Aleksuo" target="_blank" rel="noopener noreferrer"><i className="fab fa-github" ></i></a>
       <i className="fab fa-linkedin"></i>
       <i className="far fa-file"></i>
