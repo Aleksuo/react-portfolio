477dec0541322cbb493fd6310bfc7301d40eb916
Aleksuo <aleksi.suoranta@gmail.com>
2020-05-27 17:54:22 +0300

    added a few tests and fixed paths

--- src/Components/ProjectsPage.js
+++ src/Components/ProjectsPage.js
@@ -1,4 +1,4 @@
-import React from './node_modules/react'
+import React from 'react'
 import Section from './Section'
 import Card from './Card'
 
--- src/Hooks/UseViewPort.js
+++ src/Hooks/UseViewPort.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from './node_modules/react';
+import { useState, useEffect } from 'react';
 
 
 function useViewPort() {
