8aaf08bd29759f22d6e525e64ef64cc4d5a2d206
Aleksuo <aleksi.suoranta@gmail.com>
2020-05-27 19:19:27 +0300

    navbar test

---
+++ src/components/Navbar.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import Navbar from './Navbar'
+
+test('renders content' ,() => {
+    const component = render(
+        <Navbar></Navbar>
+    )
+    expect(component.container).toHaveTextContent("Home")
+    expect(component.container).toHaveTextContent("Projects")
+    expect(component.container).toHaveTextContent("Contact")
+})
